1a35efa7ee34ff1173ae22cc4902007d086da834
clifford <clifford@noukod.com>
2018-09-19 20:11:38 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,6 +10,10 @@ end
 
 
 def countdown_with_sleep
-
-
-end
+  while seconds > 0
+      puts "#{seconds} SECOND(S)!"
+      seconds -= 1
+      sleep(1)
+    end
+    return "HAPPY NEW YEAR!"
+  end
